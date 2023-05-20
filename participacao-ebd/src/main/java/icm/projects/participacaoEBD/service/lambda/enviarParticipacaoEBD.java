9c69ec9d8599549e1d3e883072c774b66c92a0c3
Alberto Paulino Rodrigues <albertoapr@MacBook-Air-de-Alberto.local>
2023-05-19 22:23:44 -0300

    construindo classe para criar funcao serverless no AWS lambda

--- participacao-ebd/src/main/java/icm/projects/participacaoEBD/service/lambda/enviarParticipacaoEBD.java
+++ participacao-ebd/src/main/java/icm/projects/participacaoEBD/service/lambda/enviarParticipacaoEBD.java
@@ -3,6 +3,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.google.gson.JsonObject;
 
+import icm.projects.participacaoEBD.ParticipacaoEBD;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -15,9 +17,13 @@ public class enviarParticipacaoEBD implements RequestStreamHandler{
         JsonObject responseJson = new JsonObject();
         JsonObject responseBody = new JsonObject();
         JsonObject headerJson = new JsonObject();
+        
+        String urlResposta  = "https://docs.google.com/document/export?format=txt&id=1c2CyPDuJB87XmnTchLR2PFqfSjqNzCy32jozXYfu7O8";
+		String urlParticipantes = "https://docs.google.com/spreadsheets/d/1zCY9DtKMQPF5s_YHXaM0TGFi8YKNdZdQja_22GnUeZQ/export?format=csv&id=1zCY9DtKMQPF5s_YHXaM0TGFi8YKNdZdQja_22GnUeZQ&gid=532508908";
+		String resultadoDoEnvio =  ParticipacaoEBD.enviarParticipacoes(urlResposta,urlParticipantes);
 
         //propriedade mensagem na resposta
-        responseBody.addProperty("message", "it's everything OK here!");
+        responseBody.addProperty("message", resultadoDoEnvio);
 
         // um header aleatório na resposta para API Gateway
         headerJson.addProperty("x-custom-header", "my custom header value");
